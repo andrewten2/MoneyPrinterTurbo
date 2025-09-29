01cafadda4b4b0619158ee8dc1fcaa396247554c
andrewten2 <58816578+andrewten2@users.noreply.github.com>
2025-09-29 21:16:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from app.config import config
 
 if __name__ == "__main__":
     logger.info(
-        "start server, docs: http://127.0.0.1:" + str(config.listen_port) + "/docs"
+        "start server, docs: http://127.0.0.1:" + str(config.listen_port) + "/docs_2
+        "
     )
     uvicorn.run(
         app="app.asgi:app",
